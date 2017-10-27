0c0a324985ddfe895382941d69c11b6457af5df0
Andrey Godlevskiy <darkday8@bigmir.net>
2017-10-27 07:19:10 +0300

    add more coments and class diagram

--- ConsoleDrivingGodlevskiy/ConsoleDrivingGodlevskiy.cpp
+++ ConsoleDrivingGodlevskiy/ConsoleDrivingGodlevskiy.cpp
@@ -18,7 +18,7 @@ int main()
 	int rightX = 0;
 
 	draw.ShowCursor(false);
-
+	//добавление в список начальных бардюров
 	for (int i = maxLines - 1; i >= 0; i--)
 	{
 		addNewBorder(borderlist, leftX, rightX, i);
@@ -26,13 +26,18 @@ int main()
 
 	while (true)
 	{
+		//рабочый цыкл когда пауза не активна
 		while (!control.Pause(&draw))
-		{			
+		{
+			//контроль за клавишей выхода из игры
 			control.Exit();
+			//чистим экран
 			system("cls");
 
+			//перебор всех бардюров на дороге
 			for (int i = 0; i < maxLines * 2; i++)
-			{				
+			{	
+				//проверка на столкновение машины с бордюром
 				if ((myCar.GetX() <= borderlist.GetElem(i)->data->GetX()) &&
 					(myCar.GetX() + myCar.GetViewHeight() - 2 >= borderlist.GetElem(i)->data->GetX()) &&
 					(myCar.GetY() <= borderlist.GetElem(i)->data->GetY()) &&
@@ -40,13 +45,17 @@ int main()
 				{
 					GameOver(draw, distance);			
 				}
-				draw.DrawGameObject(borderlist.GetElem(i)->data, maxLines);					
+				//отрисовка всех бардюров
+				draw.DrawGameObject(borderlist.GetElem(i)->data, maxLines);	
+				//изменение высоты (симул€ци€ движени€) дороги
 				borderlist.GetElem(i)->data->SetY(borderlist.GetElem(i)->data->GetY() + 1);
 			}
 
 			draw.SetColor(Yellow, Black);
+			//перебор всех стен на дороге
 			for (int i = 0; i < wallList.GetCount(); i++)
 			{
+				//проверка на столкновение машины со стеной
 				if ((myCar.GetX() <= wallList.GetElem(i)->data->GetX() + wallList.GetElem(i)->data->GetViewHeight() - 2) &&
 					(myCar.GetX() + myCar.GetViewHeight()  - 2 >= wallList.GetElem(i)->data->GetX()) &&
 					(myCar.GetY() <= wallList.GetElem(i)->data->GetY() + wallList.GetElem(i)->data->GetViewWidht() - 1) &&
@@ -54,33 +63,43 @@ int main()
 				{
 					GameOver(draw, distance);				
 				}
+				//отрисовка стены
 				draw.DrawGameObject(wallList.GetElem(i)->data, maxLines);
+				//изменение высоты (симул€ци€ движени€) стены
 				wallList.GetElem(i)->data->SetY(wallList.GetElem(i)->data->GetY() + 1);
+				//удалени€ стены из списка при выходе за экран
 				if (wallList.GetElem(i)->data->GetY() > maxLines) wallList.Del(i);
 			}
-			draw.SetColor(White, Black);	
+			draw.SetColor(White, Black);
+			//отрисовка автомобил€ игрока
 			DrawCar(draw, myCar, maxLines);
+			//отрисовка пользовательского интерфейса
 			DrawInterface(draw, myCar, distance);
 
+			//”даление бардюров, которые вышли за экран
 			borderlist.DelTail();
 			borderlist.DelTail();
+			//добавление новых бардюров
 			addNewBorder(borderlist, leftX, rightX, 0);
+			//по€ление новой стены на дороге
 			if (rand() % 100 < 15) wallList.AddHead(new Wall(rand() % 45 + 10, -3));
-
+			//контроль за клавишали управлени€ машиной игрока
 			control.CarControl(&myCar);
-
+			//пауза при отрисовке на консоле
 			Sleep(500 - 30 * myCar.GetSpeed());
+			//увеличиваем счЄтчик пройденной дистанции
 			distance++;
 		}
+		//контроль за клавишей выхода из игры
 		control.Exit();
+		//задержка при паузе
 		Sleep(500);
 	}
-
-	system("pause");
 }
 
 void addNewBorder(List &borderlist, int &leftX, int &rightX, int y)
 {
+	//эмул€ци€ неровной дороги
 	if (rand() % 10 > 6 && leftX < 10) leftX++;
 	else if (rand() % 10 > 6 && leftX > 0) leftX--;
 	borderlist.AddHead(new Border(2 + leftX, y));
